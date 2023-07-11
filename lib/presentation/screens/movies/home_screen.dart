3847329d6eac8c2762ec76ae04b42ecf062d80c6
Juan Sapién <juansapienflores@gmail.com>
2023-07-11 13:17:58 -0700

    Haciendo movieHorizontalView

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cinemapedia_app/presentation/providers/providers.dart';
+import 'package:flutter_cinemapedia_app/presentation/widgets/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -10,46 +10,48 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _HomeView()
+    return const Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
 
 class _HomeView extends ConsumerStatefulWidget {
-
   const _HomeView();
 
   @override
   _HomeViewState createState() => _HomeViewState();
-
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
-    
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
-  }
 
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
 
     final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    
-    if ( nowPlayingMovies.length == 0 ) return CircularProgressIndicator();
+    final slideShowMovies = ref.watch( moviesSlideshowProvider );
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder:(context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-          
-        );
-      },);
+    return Column(
+      children: [
+
+        const CustomAppbar(),
+
+        MoviesSlideshow(movies: slideShowMovies),
+
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subtitle: 'Martes 11',
+        )
+
+      ],
+    );
   }
 }
